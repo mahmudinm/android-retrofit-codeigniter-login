3e70e1c8b3017e620db0eadf3de867a5d83462bd
mahmudin muttaqin <claxmanx@gmail.com>
2018-12-23 17:51:46 +0700

    berhasil cek auth

--- app/src/main/java/com/example/mahmudinm/androidcodeigniterlogin/CekFragment.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterlogin/CekFragment.java
@@ -6,6 +6,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.mahmudinm.androidcodeigniterlogin.api.ApiRequest;
+import com.example.mahmudinm.androidcodeigniterlogin.api.Retroserver;
+import com.example.mahmudinm.androidcodeigniterlogin.api.response.ResponseStatus;
+import com.example.mahmudinm.androidcodeigniterlogin.utils.SessionManager;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -13,7 +24,10 @@ import android.view.ViewGroup;
  */
 public class CekFragment extends Fragment {
 
-
+    Button cekAuth;
+    SessionManager session;
+    String status ;
+    
     public CekFragment() {
         // Required empty public constructor
     }
@@ -23,7 +37,39 @@ public class CekFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cek, container, false);
+        View x = inflater.inflate(R.layout.fragment_cek, container, false);
+
+        session = new SessionManager(getActivity());
+
+        cekAuth = (Button) x.findViewById(R.id.cekAuth);
+
+        cekAuth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ApiRequest api = Retroserver.getClient().create(ApiRequest.class);
+                Call<ResponseStatus> getMain = api.getMain(session.getKeyToken());
+//                Call<ResponseStatus> getMain = api.getMain("cobaerror");
+                getMain.enqueue(new Callback<ResponseStatus>() {
+                    @Override
+                    public void onResponse(Call<ResponseStatus> call, Response<ResponseStatus> response) {
+                        status = response.body().getStatus();
+                        if (status.equals("ok")) {
+                            Toast.makeText(getActivity(), status, Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), status, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseStatus> call, Throwable t) {
+                        Toast.makeText(getActivity(), "This Error", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            }
+        });
+
+        return x ;
     }
 
 }
--- app/src/main/java/com/example/mahmudinm/androidcodeigniterlogin/api/ApiRequest.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterlogin/api/ApiRequest.java
@@ -1,10 +1,13 @@
 package com.example.mahmudinm.androidcodeigniterlogin.api;
 
 import com.example.mahmudinm.androidcodeigniterlogin.api.response.ResponseAuth;
+import com.example.mahmudinm.androidcodeigniterlogin.api.response.ResponseStatus;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 /**
@@ -17,5 +20,10 @@ public interface ApiRequest {
     @POST("auth")
     Call<ResponseAuth> authPost(@Field("username") String username,
                                 @Field("password") String password);
+
+    @GET("main")
+    Call<ResponseStatus> getMain(@Header("Authorization") String token);
+
+
 
 }
