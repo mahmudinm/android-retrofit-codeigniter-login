f4b72d7f8a2effbf0da2b8e33b63da9a10cbfc14
mahmudin muttaqin <claxmanx@gmail.com>
2018-12-23 12:31:20 +0700

    setup session manager

--- app/src/main/java/com/example/mahmudinm/androidcodeigniterlogin/LoginActivity.java
+++ app/src/main/java/com/example/mahmudinm/androidcodeigniterlogin/LoginActivity.java
@@ -2,12 +2,18 @@ package com.example.mahmudinm.androidcodeigniterlogin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    EditText txtUsername, txtPassword;
+    Button login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
